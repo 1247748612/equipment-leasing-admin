ef5bfadf87e6ccf75f97b51988efbadada598dfa
dengqing <1247748612@qq.com>
2020-05-31 13:17:07 +0800

    没过改变

--- src/components/Crud/TypeComponents/index.ts
+++ src/components/Crud/TypeComponents/index.ts
@@ -1,7 +1,5 @@
 export default function findComponent() {
   const files = require.context('.', false, /\.vue$/)
-  const files1 = require.context('./', false)
-  const files2 = require.context('..', false)
   const components: any = {
   }
   files.keys().forEach(
